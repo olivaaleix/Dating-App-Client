4dc91fea19b7e97f714f562d27117747b2fd9ea7
Aleix Oliva <aolivga@gmail.com>
2021-02-06 13:06:18 +0100

    Error tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MembersDetailsComponent } from './members/members-details/members-details.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SharedModule } from './_modules/shared.module';
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    MembersDetailsComponent
+    MembersDetailsComponent,
+    TestErrorsComponent
   ],
   imports: [
     BrowserModule,
